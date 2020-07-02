ea39872009296d55dd9080274b6f6b55ca9d02fb
jkinobe <jkinobe@gmail.com>
2020-07-02 15:30:26 +0300

    Vue Components

--- main.js
+++ main.js
@@ -1,32 +1,9 @@
-app = new Vue({
-    el: '#root',   //mounting vue to our root div so that it can be used
-    data: {
-        tasks:[
-        
-            {description: "Go to the store",completed: true },
-            {description: "Finish screencast",completed: false },
-            {description: "Make donation",completed: false },
-            {description: "Clear inbox",completed: false },
-            {description: "Make dinner",completed: false },
-            {description: "Clean room",completed: true },
-            
-        ],
-        
-},
-methods: {
-    completeme(){
-        alert("hello click");
-        this.tasks.completed=true;
-    }
-},
+Vue.component('task',{
+    template: '<li><slot></slot></li>'
+})
 
-computed: {
-    incompleteTasks() {
-
-        return this.tasks.filter(task => ! task.completed);
-
-    }
+new Vue({
     
-}
-    
+    el: '#root',
+
 });
