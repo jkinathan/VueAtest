f7b6e999f8502e84a9ecff6b54fbbbe8f29667f0
jkinobe <jkinobe@gmail.com>
2020-07-02 15:55:02 +0300

    component template

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
+Vue.component('task-list',{
+    
+    template: `
+        <div>
+            <task v-for="tas in tasks">{{ tas.task }}</task>
+        </div>
+        
+        `,
+
+    data() {
+        return {
+            tasks:[
+                { task: 'Go to the shores', completed: true},
+                { task: 'Go to the mall', completed: false},
+                { task: 'Go to the farm', completed: true},
+                { task: 'Go to work', completed: false},
+                
+            ]
+        }
+    },
+});
+
 Vue.component('task',{
     template: '<li><slot></slot></li>'
-})
+});
+
+
+
+
 
 new Vue({
     
