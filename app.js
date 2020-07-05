5464f445f665ca4fa4c553d478dfb2253b01fb7c
jkinobe <jkinobe@gmail.com>
2020-07-05 14:09:01 +0300

    eventing

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ new Vue({
 
   data: {
     name: "Jordy",
+    age:25,
     job: "Ninja",
     website: "https://github.com/jkinathan",
     websiteTag:'<a href="https://github.com/jkinathan">jordan Github</a>',
   },
   methods:{
+    addyear(){
+
+      this.age= this.age+1;
+
+    },
+    subyear(){
+
+      this.age= this.age-1;
+
+    },
     greet(time){
 
       return "Good "+time+" "+this.name;
