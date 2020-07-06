791f9f4598ca24aeedf34ac416e67f1bd320cedb
jkinobe <jkinobe@gmail.com>
2020-07-06 15:57:52 +0300

    computed properties from methods

--- app.js
+++ app.js
@@ -2,26 +2,38 @@ new Vue({
   el:"#vue-app",
 
   data: {
+    a:0,
+    b:0,
     name2: "",
     age2: "",
     name: "Jordy",
     x:0,
     y:0,
-    age:25,
+    age:20,
     job: "Ninja",
     website: "https://github.com/jkinathan",
     websiteTag:'<a href="https://github.com/jkinathan">jordan Github</a>',
+  },
+  computed:{
+    addToA(){
+      console.log("addToA");
+      return this.a + this.age;
+    },
+    addToB(){
+      console.log("addToB");
+      return this.b + this.age;
+    },
   },
   methods:{
     clicker(){
       alert("You Clicked me");
     },
-    logname(){
-
-    },
-    logage(){
-       
-    },
+    // addToA(){
+    //   return this.a + this.age;
+    // },
+    // addToB(){
+    //   return this.b + this.age;
+    // },
     updatexy(event){
       //console.log(event);
       this.x = event.offsetX;
