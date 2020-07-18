e4418a50f118667967c40b22394134c5e3450aec
jkinobe <jkinobe@gmail.com>
2020-07-18 17:51:26 +0300

    vue components from laracasts

--- bulma.js
+++ bulma.js
@@ -1,30 +1,52 @@
-Vue.component("message",{
-    props:[
-        "title","body"
-    ],
-    data() {
-        return {
-            isVisible: true
-        }
-    },
+Vue.component("modal",{
     template: `
-    <article class="message" v-show="isVisible">
-        <div class="message-header">
-            <p>{{ title }}</p>
-            <button class="delete" aria-label="delete" v-on:click="hideModal"></button>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            
+            <div class="box">
+                <slot></slot>
+            </div>
+                
         </div>
-        <div class="message-body">
-            {{ body }}   
-        </div>
-    </article>
-    `,
-    methods: {
-        hideModal(){
-            this.isVisible = false
-        }
-    },
-}),
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+    `
+ }),
+// ,
+// Vue.component("message",{
+//     props:[
+//         "title","body"
+//     ],
+//     data() {
+//         return {
+//             isVisible: true
+//         }
+//     },
+//     template: `
+//     <article class="message" v-show="isVisible">
+//         <div class="message-header">
+//             <p>{{ title }}</p>
+//             <button class="delete" aria-label="delete" v-on:click="hideModal"></button>
+//         </div>
+//         <div class="message-body">
+//             {{ body }}   
+//         </div>
+//     </article>
+//     `,
+//     methods: {
+//         hideModal(){
+//             this.isVisible = false
+//         }
+//     },
+// }),
+
 
 new Vue({
-    el: "#root"
+    el: "#root",
+    data: {
+
+        showmodal: false
+        
+    },
 });
