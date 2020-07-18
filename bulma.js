daf97fdd758e5bd8cc018be349b63c5e0471dec0
jkinobe <jkinobe@gmail.com>
2020-07-18 16:17:48 +0300

    bulma components

---
+++ bulma.js
@@ -0,0 +1,18 @@
+Vue.component("message",{
+    
+    template: `
+    <article class="message">
+        <div class="message-header">
+            <p>{{ title }}</p>
+            <button class="delete" aria-label="delete"></button>
+        </div>
+        <div class="message-body">
+            {{ body }}   
+        </div>
+    </article>
+    `
+}),
+
+new Vue({
+    el: "#root"
+});
